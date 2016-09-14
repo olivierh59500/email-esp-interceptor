9d992a98d1e80db5edb2e48c4b275ed39dba29f8
syymza <syymza@gmail.com>
2016-09-14 12:15:56 +0100

    Do not save preview emails

--- app/services/transmissionService.js
+++ app/services/transmissionService.js
@@ -1,7 +1,12 @@
 const config = require('../../config');
-const fetcher = require('./_fetcher')(config.sendServiceHost, config.sendServiceAuth);
+const fetcherF = require('./_fetcher');
+const sendServiceHost = config.sendServiceHost;
+const sendServiceAuth = config.sendServiceAuth;
+const testSendServiceAuth = config.testSendServiceAuth;
 
-function* createTransmission(transmissionBody) {
+function* createTransmission(transmissionBody, isPreview = false) {
+  const auth = isPreview ? testSendServiceAuth : sendServiceAuth;
+  const fetcher = fetcherF(sendServiceHost, auth);
   return yield fetcher.createResource('/send-by-address', transmissionBody);
 }
 
