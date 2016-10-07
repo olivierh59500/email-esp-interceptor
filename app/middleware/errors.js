b5d2eed94e7c914ee1b5ce59fd39d8aed1a2bef9
Andrew Snead <andrewcsnead@gmail.com>
2016-10-07 18:56:22 +0100

    Update errors.js

--- app/middleware/errors.js
+++ app/middleware/errors.js
@@ -6,7 +6,6 @@ function notFound(req, res, next) {
 
 // eslint-disable-next-line
 function errorMiddleware(err, req, res, next) {
-  console.log(err);
   res.status(err.status || err.statusCode || 500);
   const errors = err.errors && err.errors.length ? err.errors : [{ message: err.message }];
   return res.json({ errors });
