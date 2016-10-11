f0b4cf3b2c5063a2389d98e8723bb4255f44071f
snozza <andrewcsnead@gmail.com>
2016-10-11 12:24:56 +0100

    add recipients list endpoint

--- app/controllers/recipientListController.js
+++ app/controllers/recipientListController.js
@@ -2,6 +2,10 @@ const substitutionService = require('../services/substitutionService');
 const listConverter = require('../utils/listConverter');
 const handleError = require('../utils/handleError');
 
+function substitutionError(err) {
+  return handleError(err.message, err.status, err.errors);
+}
+
 function* read(req, res, next) {
   const recipientListId = req.params.recipientListId;
   const showRecipients = !(req.query.show_recipients === 'false');
@@ -10,7 +14,7 @@ function* read(req, res, next) {
     const recipientList = listConverter.substitutionToRecipientList(substitution, showRecipients);
     res.json(recipientList);
   } catch (err) {
-    next(handleError(err.message, err.status));
+    next(substitutionError(err.message, err.status));
   }
 }
 
@@ -23,11 +27,28 @@ function* create(req, res, next) {
     // & total_accepted_recipients
     res.json({ results: { id: substitutionRes._id, name: substitutionRes.name } });
   } catch (err) {
-    next(handleError(err.message, err.status));
+    next(substitutionError(err.message, err.status));
   }
+}
+
+function* list(req, res, next) {
+  let lists;
+  try {
+    lists = yield substitutionService.fetchSubstitutions();
+  } catch (err) {
+    return next(substitutionError(err));
+  }
+
+  return res.json({
+    results: lists.map(recipientList => ({
+      id: recipientList._id,
+      name: recipientList.name,
+    }))
+  });
 }
 
 module.exports = {
   create,
-  read
+  read,
+  list
 };
